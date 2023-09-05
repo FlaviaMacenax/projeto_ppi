89ed492442bc77995bd8510e3921ecb66b1587e1
Sarah  Lima <lima.sarah@escolar.ifrn.edu.br>
2023-09-05 10:56:44 -0300

    Iniciei o CSS da página de pagamento

--- dadospag.php
+++ dadospag.php
@@ -32,49 +32,47 @@ if(isset($_POST['pagar'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel= "stylesheet" type= "text/css" media="screen" href="main-css.css">
-    <title>Usuario</title>
+    <!--Importação da fonte-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=ADLaM+Display&family=Josefin+Sans:wght@300;400;500;700&family=Prata&display=swap" rel="stylesheet">
+    <!--Ícones do footer-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--Ícone da barra do navegador-->
+    <link rel="icon" type="image/x-icon" href="imagens/flexicon.png">
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="isiarah.css">
+    <title>Pagamento</title>
+    <style>
+         body{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            background-image: url(imagens/fundo2.png);
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
 </head>
+
 <body>
-<table>
-    <tr>
-        <th colspan="2">Dados do Usuário</th>
-    </tr>
-    <tr>
-        <td>Usuário:</td>
-        <td><?php echo $usuario; ?></td>
-    </tr>
-    <tr>
-        <td>Nome completo:</td>
-        <td><?php echo $nome; ?></td>
-    </tr>
-    <tr>
-        <td>E-mail:</td>
-        <td><?php echo $email; ?></td>
-    </tr>
-    <tr>
-        <td>Telefone:</td>
-        <td><?php echo $telefone; ?></td>
-    </tr>
-    <tr>
-        <td>Endereço:</td>
-        <td><?php echo $endereco; ?></td>
-    </tr>
-</table>
 
+    <h2><i class="fa-solid fa-user fa-beat icon"></i>Dados do usuário</h2>
+        <p>Usuário: <?php echo $usuario; ?></p>
+        <p>Nome completo: <?php echo $nome; ?></p>
+        <p>E-mail: <?php echo $email; ?></p>
+        <p>Telefone: <?php echo $telefone; ?></p>
+        <p>Endereço: <?php echo $endereco; ?></p>
+      
 <form action="dadospag.php" method="post">
-
-<br>
-
-<table>
-
-    <tr>
-        <th colspan = "4">Dados do Pagamento</th>
-    </tr>
-    <tr>
-        <th>#</th>
-        <th>Métodos de Pagamento</th>
-        <th colspan = "2">Dados da Cobrança</th>
+        <h2><i class="fa-solid fa-dollar-sign fa-beat"></i>Dados do Pagamento</h2>
+    
+        <p>#</p>
+        <p>Métodos de Pagamento</p>
+        <p>Dados da Cobrança</p>
     </tr>
     <tr>
         <td><input type = "radio" name = "metpag" value = "deb-avista"></td>
@@ -106,5 +104,60 @@ if(isset($_POST['pagar'])) {
 
 </form>
 
+<!--Rodapé-->
+<footer>
+            <div id="footer_content">
+                <div id="footer_contacts">
+                    <h1><img src="imagens/logo.png"  width="150" height="auto" alt="logo do quiz"></h1>
+                    <p class="redes">Siga nossas redes sociais:</p>
+ 
+                    <div id= "footer_social_media">
+                        <a href="https://www.instagram.com/" class="footer-link" id="instagram">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+ 
+                        <a href="https://www.youtube.com/" class="footer-link" id="youtube">
+                            <i class="fa-brands fa-youtube"></i>
+                        </a>
+ 
+                        <a href="https://twitter.com/" class="footer-link" id="x">
+                            <i class="fa-brands fa-x-twitter"></i>
+                        </a>
+                    </div>
+                </div>
+ 
+                <ul class="footer-list">
+                    <li>
+                        <h3>Desenvolvido por</h3>
+                    </li>
+                    <li>
+                        <a href="mailto:flavia.macena@escolar.ifrn.edu.br" class="footer-link" target="blank">Flávia Jamily</a>
+                    </li>
+                    <li>
+                        <a href="mailto:isabel.natalia@escolar.ifrn.edu.br" class="footer-link" target="blank">Isabel Natalia</a>
+                    <li>
+                        <a href="mailto:lima.sarah@escolar.ifrn.edu.br" class="footer-link" target="blank">Sarah Lima</a>
+                    </li>
+                </ul>
+ 
+                <ul class="footer-list">
+                    <li>
+                        <h3>Ficha técnica</h3>
+                    </li>
+                    <li>
+                        <p>Trabalho realizado para a disciplina de programação para internet</p>
+                    </li>
+                    <li>
+                        <p>Professor: Éberton Marinho</p>
+                    </li>
+                    <li>
+                        <p>Unidade 2</p>
+                    </li>
+                    <li>
+                        <p>IFRN- Campus Canguaretama</p>
+                    </li>
+                </ul>
+            </div>
+        </footer>
 </body>
 </html>
